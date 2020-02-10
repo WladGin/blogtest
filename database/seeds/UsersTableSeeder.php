eb444e8d4f3362b527576e447abd24e6f70bc884
WladGin <wladgin@gmail.com>
2020-02-10 17:29:20 +0200

    create project and create migrations, seeds, models

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'Автор не известен',
+                'email' => 'author_unknown@g.g',
+                'password' => bcrypt(Str::random(16)),
+            ],
+            [
+                'name' => 'Автор',
+                'email' => 'author@g.g',
+                'password' => bcrypt(Str::random('123123')),
+            ],
+        ];
+
+        DB::table('users')->insert($data);
+    }
+}
