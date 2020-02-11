f65b753eb420889906df9e7c4a3b1002c9ce5c20
WladGin <wladgin@gmail.com>
2020-02-11 13:29:18 +0200

    create auth controllers, post and base controllers, install nmp, gulp

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-02-10 14:36:18.
+    * Generated on 2020-02-11 10:22:58.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.14.0 on 2020-02-10 14:36:17.
+ * Generated for Laravel 6.14.0 on 2020-02-11 10:22:57.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
